b2d622ce9c8e426f8d36eb77780b077d6c93198f
aaayush2604 <aayushfabwani2@gmail.com>
2024-01-27 21:59:25 +0530

    Icons for Genre Added

--- src/BookReviews/BookReviews.jsx
+++ src/BookReviews/BookReviews.jsx
@@ -1,12 +1,12 @@
 import './BookReviews.css';
 import data from './data.json';
+import {FaBeer, FaStar} from "react-icons/fa";
 
 
-const ReviewObject=({Title,Author,Image})=>{
-  console.log(Title);
+const ReviewObject=({Title,Author,Image,Genre})=>{
+
     return<>
       <div className="BookCard">
-        {console.log(Image)}
     <img src={Image} className="BookCardInner"/>
       <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
     <div className="TextBox">
@@ -17,15 +17,9 @@ const ReviewObject=({Title,Author,Image})=>{
         {Author}
       </h5>
       <div className="GenreContainer">
-      <img alt="Tania Andrew"
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-        className="Genre" />
-      <img alt="Tania Andrew"
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-        className="Genre" />
-      <img alt="Tania Andrew"
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-        className="Genre" />
+        {Genre.map((gen)=>{
+          return <img alt={gen} src={gen+'.png'} className='Genre'/>
+        })}
       </div>
     </div>
   </div>  
@@ -36,7 +30,7 @@ const ReviewObjectList=()=>{
     return <main className='ObjectGrid'>
       {
         data.map((Book,index)=>{
-          return <ReviewObject  key={index} Title={Book.Title} Author={Book.Author} Image={Book.Image}/>
+          return <ReviewObject  key={index} Title={Book.Title} Author={Book.Author} Image={Book.Image} Genre={Book.Genre}/>
         })
       }
     </main>
